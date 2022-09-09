d250c94c9ac65142e71e91c2e0a6a34a3b438e84
sametbakmaz <sametbkmz@gmail.com>
2022-09-09 23:52:44 +0300

    Initial Commit

---
+++ HaritaUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaritaUygulamasi
+//
+//  Created by Abdulsamet Bakmaz on 9.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
