ecb363871b0e97977591ea2128d488ac1a579acc
sametbakmaz <sametbkmz@gmail.com>
2022-09-13 02:49:29 +0300

    code review

--- HaritaUygulamasi/ListViewController.swift
+++ HaritaUygulamasi/ListViewController.swift
@@ -6,24 +6,80 @@
 //
 
 import UIKit
-
-class ListViewController: UIViewController {
-
+import CoreData
+//yapmamız gereken ilk şey list view ayarlarını yapmak classta kütüphaneleri çağırmak.
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var isimDizisi = [String]()
+    var idDizisi = [UUID]()
+    var secilenYerIsmi = ""
+    var secilenYerId : UUID?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //core datadan veri çekeceğiz bunun içinde fetchRequest işlemi gerçekleştireceğiz
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(artiButtonTiklandi))//sağ üst tarafta bir + butonu için. Methoddaki action kısmı perform segue yapacağım kısım
+        veriAl()
     }
     
+    func veriAl(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Yer")//requestimizi oluşturuyoruz
+        request.returnsObjectsAsFaults = false
+        do{
+            //neyi deneyeceğiz context.fetch bize sonuçların olduğu bir dizi veriyordu ve any dizisi veriyordu. Biz NSManagedObject haline getirmek istiyorduk bu yüzden bir try catch işlemi yapacağız
+            let sonuclar = try context.fetch(request)
+            if sonuclar.count > 0{
+                isimDizisi.removeAll(keepingCapacity: false) //diziye girmeden içini boşaltıp kapasite tutmamasını sağlıyoruz
+                idDizisi.removeAll(keepingCapacity: false)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                for sonuc in sonuclar as! [NSManagedObject]{ //sonuclar any geliyor ve bunu NSmanagedObject olarak cast ettik
+                    if let isim = sonuc.value(forKey: "isim") as? String{
+                        isimDizisi.append(isim)
+                    }
+                    if let id = sonuc.value(forKey: "id") as? UUID{
+                        idDizisi.append(id)
+                    }
+                }
+                tableView.reloadData()//işimiz bittikten sonra tableViewımızı yeniliyoruz
+            }
+        }catch{
+            print("hata")
+        }
     }
-    */
+    @objc func artiButtonTiklandi(){//tıklandığında yapılacak perform segue kodları
+        //eğer hiç bir yere tıklanmazsa ve artı butonuna tıklanırsa perform segue yapmadan önce seçilen yer ismi boş gitsin ve boş ise yeni yer eklemeye geldiği belli olsun
+        secilenYerIsmi = " "
+        performSegue(withIdentifier: "toMapsVC", sender: nil)
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isimDizisi.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = isimDizisi[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { //tableView'da herhangi bir rowa tıklandıysa ne yapacağız
+        secilenYerIsmi = isimDizisi[indexPath.row]
+        secilenYerId = idDizisi[indexPath.row] //böylece seçilen indexleri burada atamış oldum
+        performSegue(withIdentifier: "toMapsVC", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) { //segue ye hazırlan
+        if segue.identifier == "toMapsVC"{
+            let destinationVC = segue.destination as! MapsViewController //gideceğim segueyi tanımladım
+            destinationVC.secilenIsim = secilenYerIsmi
+            destinationVC.secilenId = secilenYerId //böylece veri aktarımını gerçekleştirmiş olucam. Buradan sonra MapsViewController'a gidip seçilen isim boş değilse diye oluşturduğumuz ife gidiyorum
+        }
+    }
 }
--- HaritaUygulamasi/MapsViewController.swift
+++ HaritaUygulamasi/MapsViewController.swift
@@ -19,6 +19,14 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
     var locationManager = CLLocationManager()//konumla ilgili yönetim için CLLocationManager sınıfından değişken yarattık
     var secilenLatitude = Double()
     var secilenLongitude = Double()//enlem ve boylamı alabilmemiz için double bir değişken oluşturmamız gerekiyordu ki core datada double oluşturduğumuz için setlemeyi böyle bekliyor
+    
+    var secilenIsim = ""//diğer taraftan prepare for sehue ile bunlara ulaşabilmek için 2 adet değişken oluşturduk
+    var secilenId : UUID?
+
+    var annotationTitle = ""
+    var annotationSubtitle = ""
+    var annotationLatitue = Double()
+    var annotationLongitude = Double()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -30,6 +38,62 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
         let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(konumSec(gestureRecognizer:)))//jest algılayıcıları kullanarak uzun tıklanan yere pin koyma işaret koyma fonksiyonu -> uzun tıklama yaptığımız anlaması için longPresGestureRecognizer kullandık
         gestureRecognizer.minimumPressDuration = 3//minimum basılı tutma, basma süresi
         mapView.addGestureRecognizer(gestureRecognizer)
+        if secilenIsim != ""{
+            //core data dan verileri çek
+            if let uuidString = secilenId?.uuidString{//eğer bunu alabiliyorsam uuid yi consola bastım - secilenId? diyerek gelen değeri optional olmaktan çıkartıyor
+                print(uuidString)
+                //bu uuid ye göre bir filtreleme yaparak veri çekeceğiz. Yine fetchRequest oluşturacağız sadece bir NSPredicate oluşturarak veri çekeceğiz
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Yer")//çekeceğimiz tablo
+                fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)//hangi kolonu neyle filtreleyeceğimizi yazıyoruz. formata yazdığımız "id = %@" -> id si birazdan yazacağım argümanlara eşit olanı getir anlamında kullanılıyor
+                fetchRequest.returnsObjectsAsFaults = false
+                do{
+                    let sonuclar = try context.fetch(fetchRequest)
+                        if sonuclar.count > 0 {
+                            for sonuc in sonuclar as! [NSManagedObject] {
+                                if let isim = sonuc.value(forKey: "isim") as? String{
+                                    annotationTitle = isim //yukarıda hepsinin değişkenini tanımladım istediğim yerden erişebilmek için
+                                    if let not = sonuc.value(forKey: "not") as? String{
+                                        annotationSubtitle = not
+                                        if let latitude = sonuc.value(forKey: "latitude") as? Double{
+                                            annotationLatitue = latitude
+                                            if let longitude = sonuc.value(forKey: "longitude") as? Double{
+                                                annotationLongitude = longitude
+                                                
+                                                //anotasyon koyma, gösterme
+                                                let annotation = MKPointAnnotation()
+                                                annotation.title = annotationTitle
+                                                annotation.subtitle = annotationSubtitle
+                                                let coordinate = CLLocationCoordinate2D(latitude: annotationLatitue, longitude: annotationLongitude)
+                                                annotation.coordinate = coordinate
+                                                
+                                                mapView.addAnnotation(annotation)
+                                                isimTextField.text = annotationTitle
+                                                notTextField.text = annotationSubtitle
+                                                
+                                                locationManager.stopUpdatingLocation()//güncellemeyi bırak
+                                                
+                                                //seçtiğimiz konuma gitmesi için kod bloğu altta
+                                                let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                                let region = MKCoordinateRegion(center: coordinate, span: span)//coordinatımız zaten var ama span imiz yok onuda hemen üstünde oluşturduk
+                                                mapView.setRegion(region, animated: true)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    
+                }catch{
+                    print("hata")
+                }
+                
+            }
+        }else{
+            //yeni veri eklemeye gelmiştir
+        }
+        
     }
     @objc func konumSec(gestureRecognizer : UILongPressGestureRecognizer){//oluşturduğum gestureRecognizere fonksiyonda ulaşmam gerek
         if gestureRecognizer.state == .began{ //jest algılayıcının durumunu belirlemek için yazdık. Başladı mı kontrolünü koyduk
@@ -46,18 +110,60 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
             mapView.addAnnotation(annotation)
         }
         
-        
+    }
+    //anotasyon koyduğumuz yerde bilgi kutucuğu oluşturup sağ kısımda işaret çıkarmak için gereken kod
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {//araştır örneklerine bak
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let reuseId = "myAnnotation"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+            pinView?.tintColor = .red
+            
+            let button = UIButton(type: .detailDisclosure)
+            pinView?.rightCalloutAccessoryView = button
+        }else{
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {//gösterdiğimiz butonun üstüne tıklanınca ne olacak
+        if secilenIsim != ""{ // daha önce kaydettiğimiz bir yere baktığımıza emin olduk
+            let requestLocation = CLLocation(latitude: annotationLatitue, longitude: annotationLongitude)
+            CLGeocoder().reverseGeocodeLocation(requestLocation) { (placemarkDizisi, hata) in
+            //CLGeocoder bir arayüz kordinatlarla arayüzler arasında bağlantılar yapmaya çalışır.reverseGeocoderLocation ise geocoding işlemini tersten yapmayı sağlar navigasyonu başlatmak için gereken işlemi verecek. Completition Handler'a Entera Basarsak benden placeMark dizisi isteyecek.
+            if let placemarks = placemarkDizisi {
+                if placemarks.count > 0{
+                    let yeniPlacemark = MKPlacemark(placemark: placemarks[0])//navigasyonu açmak içinde harita öğesi oluşturmamız gerekiyordu ve nerde oluşturulacağı ile alakalı placemark istiyor bizden. Bunu bile bilmek içinde güncel konumumuz gösteren Geocoder oluşturduk
+                    let item = MKMapItem(placemark: yeniPlacemark)
+                    item.name = self.annotationTitle
+                    
+                    let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]//navigasyonu açmak
+                    item.openInMaps(launchOptions: launchOptions)
+                }
+            }
+            //CLPlacemark kullanıcının anlayabileceği bir şekilde oluşturulmuş bir tanımmış ve bir coğrafi konumun oluşturulabileceği anlayabileceği bir tanım
+        }
+    }
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) { //konumlar güncellendi fonksiyonu ve [CLLocation] bana tek tek güncellenen konumları veriyor yani bu dönen konumu benim alıp işlemem gerek yani locations[0]' ı çağırdığımda kullanabilirim
         //print(locations[0].coordinate.latitude) //enlem
         //print(locations[0].coordinate.longitude) //boylam
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)//bizden delta bekliyor ilk 0.1 0.1 vererek denedik 0.9-0.9 verseydik zoom out açılacaktı konum yani uzaktan yaklaştırılmamış hali
-        let location = CLLocationCoordinate2DMake(locations[0].coordinate.latitude,  locations[0].coordinate.longitude)//enlemi ve boylamı alıp location değişkenimde tutuyoruz. Haritada konumumuza gitmemiz için gerekli
-        let region = MKCoordinateRegion(center: location, span: span)//Center nereye merkez alıp konumlanacağı yani yukarda oluşturduğumuz location.
-        mapView.setRegion(region, animated: true) //bulunduğun bölge öncesinde regionu oluşturmam gerek
         
-        //locationManager() fonksiyonunda kısaca her güncelleme olduğunda benim haritamı ona göre güncelle dedik
+        if secilenIsim == " "{  //eğer kullanıcı yeni bir yer ekliyorsa bunu yap belirlediğimiz konumu lokasyonu al. Bu yüzden bir yer seçilip gösterilmeyeceğine emin olmak istedik
+
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)//bizden delta bekliyor ilk 0.1 0.1 vererek denedik 0.9-0.9 verseydik zoom out açılacaktı konum yani uzaktan yaklaştırılmamış hali
+            let location = CLLocationCoordinate2DMake(locations[0].coordinate.latitude,  locations[0].coordinate.longitude)//enlemi ve boylamı alıp location değişkenimde tutuyoruz. Haritada konumumuza gitmemiz için gerekli
+            let region = MKCoordinateRegion(center: location, span: span)//Center nereye merkez alıp konumlanacağı yani yukarda oluşturduğumuz location.
+            mapView.setRegion(region, animated: true) //bulunduğun bölge öncesinde regionu oluşturmam gerek
+            
+            //locationManager() fonksiyonunda kısaca her güncelleme olduğunda benim haritamı ona göre güncelle dedik
+        }//değilse boş bırakacağım else i yazmama gerek yok
     }
 
     @IBAction func kaydetTiklandi(_ sender: Any) {
@@ -71,9 +177,9 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
         yeniYer.setValue(secilenLongitude, forKey: "longitude")
         yeniYer.setValue(UUID(), forKey: "id")
         do{
-            try
+            try context.save() //kayıt edildi
             print("kayıt edildi")
-        }catch{
+        } catch {
             print("hata")
         }
     }
